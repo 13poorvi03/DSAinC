e104b5f6c27559eb1e67b27577f8564e11d4ad8a
13poorvi03 <poorvisharma0306@gmail.com>
2025-12-30 13:55:01 +0530

    added

---
+++ intro/code01.c
@@ -0,0 +1,13 @@
+// dsa-- way to arrange in main memory for efficient usage ex-- arrays, linkedlist,stacks,ques,tree,graph
+
+// algorithm--- sequence of steps to solve a given problem 
+
+//main memory --- ram(random access memory )
+
+// database-- collection of information in permanenet storage for faster retrival and updation 
+
+// data warehousing -- management of huge amount of legacy data for bettter ananlysis 
+
+//big data --- refers to writing programs that can store, process, and analyze massive, complex datasets using specialized frameworks and parallel computing techniques
+
+#include<stdio.h>
