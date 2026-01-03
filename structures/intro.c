9f0d989b3c2c829cc70ae6f6a74acdf58f429251
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-03 18:35:31 +0530

    addeded intro of structures

--- structures/intro.c
+++ structures/intro.c
@@ -15,8 +15,8 @@ struct pokemon{    //user defined data type
     int speed;      //in this we store same data of pokemeon
     int attack;     // in this we can use differt type of data type
     char tier;   // S,A,B,C,D
-};
-struct pokemon pikachu;
+} pikachu, charizard;  //we can also declare variable like this rather than writing struct pokepon pikachu;
+// struct pokemon pikachu;
 printf("enter the pikachu attack : ");
 scanf("%d",&pikachu.attack);
 // pikachu.attack=60;
@@ -25,7 +25,7 @@ pikachu.tier ='A';
   printf("%d\n",pikachu.attack);
 
   //intialization 
-struct pokemon charizard;
+// struct pokemon charizard;
     charizard.attack= 130;
     charizard.hp=80;
     charizard.speed=90;
