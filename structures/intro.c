28712bb0bae9d533de851745769d2fd920338fed
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-03 18:26:25 +0530

    intro of structures

---
+++ structures/intro.c
@@ -0,0 +1,35 @@
+// structures is user defined data type 
+
+// declaration 
+// struct employee{
+// int emp_id;
+// char name;
+// float salary;
+// };
+
+#include<stdio.h>
+int main(){
+struct pokemon{    //user defined data type
+    int hp;         // this all are a properties of pokemon 
+    int speed;      //in this we store same data of pokemeon
+    int attack;
+    char tier;   // S,A,B,C,D
+};
+struct pokemon pikachu;
+printf("enter the pikachu attack : ");
+scanf("%d",&pikachu.attack);
+// pikachu.attack=60;
+pikachu.speed=100;
+pikachu.tier ='A';
+  printf("%d\n",pikachu.attack);
+
+  //intialization 
+struct pokemon charizard;
+    charizard.attack= 130;
+    charizard.hp=80;
+    charizard.speed=90;
+    charizard.tier='S';
+  printf("%c\n",charizard.tier);
+
+return 0;
+}
