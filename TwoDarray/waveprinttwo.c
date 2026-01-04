2a33b0b5b048dc610babf96d9cd851c492e68d80
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-04 18:11:26 +0530

    added

---
+++ TwoDarray/waveprinttwo.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+   int r,c;
+   printf("enter the number of row : ");
+   scanf("%d",&r);
+   printf("enter the number of columns : ");
+   scanf("%d",&c);
+   
+   int arr[r][c];
+   
+   for(int i=0; i<r; i++){
+     for(int j =0; j<c; j++){
+      scanf("%d",&arr[i][j]);
+      }
+   }
+   
+       printf("\n");
+
+     //wave print
+
+     for(int j=0; j<c; j++){
+      if(j%2==0){
+      for(int i =r-1; i>=0; i--){
+       printf("%d ",arr[i][j]);
+      }
+     }
+      else{
+        for(int i=0; i<r; i++){
+       printf("%d ",arr[i][j]);
+        }
+      }
+        printf("\n");
+     }
+     return 0;
+}
