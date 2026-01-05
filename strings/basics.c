ee7059a0e8603e71249ec600f513b30a5ed2148f
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-05 07:24:22 +0530

    adddddddddedddddd

---
+++ strings/basics.c
@@ -0,0 +1,59 @@
+// string is nota data type in c
+// we have char , int ,float and other data type but no "strings" data type in c
+//string is not a supported data type in c but it is a very useful concept used to model real worls entities like name,city etc 
+//we express strings using an array pf character terminated by a null character
+
+// #include<stdio.h>
+// int main(){
+
+//     char ch[5]={'a','b','c','d','f'};
+   
+//             printf("%p\n",&ch[0]);
+//             printf("%p\n",&ch[1]);
+//             printf("%p\n",&ch[2]);
+//             printf("%p\n",&ch[3]);
+      
+// }
+
+
+#include<stdio.h>
+int main(){
+    // char ch ='A';  //65
+    // int x=(int)ch;
+    // printf("%d\n",x);
+    // printf("%c",ch);
+    // return 0;
+
+
+//     char ch ='0';  //48
+//     int x=(int)ch;
+//     printf("%d\n",x);
+//     printf("%c",ch);
+//     return 0;
+
+    // char ch ='a';  //67
+    // int x=(int)ch;
+    // printf("%d\n",x);
+    // printf("%c",ch);
+    // return 0;
+
+
+    // char ch ='1';  //49
+    // int x=(int)ch;
+    // printf("%d\n",x);
+    // printf("%c",ch);
+    // return 0;
+
+    char ch ='9';  //49
+    int x=(int)ch;
+    printf("%d\n",x);
+    printf("%c",ch);
+    return 0;
+}
+
+
+
+
+
+
+
