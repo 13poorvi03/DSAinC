6fa07d7426d1ee653d510b9bf83b57cb2839d423
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-06 19:51:10 +0530

    random

---
+++ Treee/code01.c
@@ -0,0 +1,4 @@
+//In this folder, I have started solving all the important questions related to Trees. 
+//It includes the basics like createNode, linking nodes, and different traversals (inorder, preorder, postorder). 
+//Each question is written with step-by-step dry runs and boundary conditions so that nothing is missed during exams. 
+//This folder will serve as a complete reference guide, making revision simple, clear, and exam-ready.
